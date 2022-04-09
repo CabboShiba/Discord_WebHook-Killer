e88bc955db1e714aa82f2cb6b797af2cc6c62876
Cabbo <92642446+CabboShiba@users.noreply.github.com>
2022-04-09 16:14:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace Discord_WebhookDeleter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = $"[{Utils.Time()}] Discord_WebHook Deleter by Cabbo";
+            Console.WriteLine($"[{Utils.Time()} Please insert a valid Discord WebHook:");
+            Console.Write($"[{Utils.Time()}] ");
+            string webhook = Console.ReadLine();
+            if (WebhookPatcher(webhook))
+            {
+                Console.WriteLine($"[{Utils.Time()}] Succesfully deleted your WebHook.");
+            }
+            else
+            {
+                Console.WriteLine($"[{Utils.Time()}] Error during deleting process. Your WebHook may be still alive.");
+            }
+            Utils.Leave();
+        }
+
+        public static bool WebhookPatcher(string webhook)
+        {
+            var req = (HttpWebRequest)WebRequest.Create(webhook);
+            req.Method = "DELETE";
+            req.KeepAlive = true;
+            try
+            {
+                var responsecheck = (HttpWebResponse)req.GetResponse();
+                responsecheck.Close();
+                if (responsecheck.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Utils/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Discord_WebhookDeleter
+{
+    internal class Utils
+    {
+        public static string Time()
+        {
+            DateTime time = DateTime.Now;
+            return time.ToString();
+        }
+        public static void Leave()
+        {
+            Console.WriteLine($"[{Utils.Time()}] Press enter to leave...");
+            Console.ReadLine();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
